e10e0bdbf3347ca30dc889c757552c7b69bf8f8b
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-08-01 13:37:13 +0200

    Initial commit

---
+++ test/cdk-appsync-serverless-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAppsyncServerlessApp from '../lib/cdk-appsync-serverless-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-appsync-serverless-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAppsyncServerlessApp.CdkAppsyncServerlessAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
