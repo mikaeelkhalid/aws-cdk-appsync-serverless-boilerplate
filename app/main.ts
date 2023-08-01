124afd6dc793c236721e980db09fa6639949cf1a
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2023-08-01 17:44:23 +0200

    add config

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,13 @@
 import 'source-map-support/register';
 import { App } from 'aws-cdk-lib';
 import { AppsyncServerlessStack } from '../stacks';
+import { config } from '../config';
+
+const { prefix } = config;
 
 const app = new App();
 
-new AppsyncServerlessStack(app, 'appsync-serverless-stack', {
+new AppsyncServerlessStack(app, `${prefix}-serverless-stack`, {
   env: {
     region: process.env.REGION,
     account: process.env.ACCOUNT,
---
+++ config/index.ts
@@ -0,0 +1,6 @@
+const prefix = 'appsync';
+
+export const config = {
+  prefix,
+};
+
